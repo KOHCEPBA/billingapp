959b152549d649a6eaa625022a092759aff7ec6f
nrudopisov <nrudopisov@neoflex.ru>
2021-05-23 00:33:07 +0400

    Added methods for query audits

--- src/main/java/ru/rudnick/billingapp/repository/AuditRepository.java
+++ src/main/java/ru/rudnick/billingapp/repository/AuditRepository.java
@@ -4,5 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.rudnick.billingapp.entity.Audit;
 import ru.rudnick.billingapp.entity.Bill;
 
+import java.util.List;
+
 public interface AuditRepository extends JpaRepository<Audit, Long> {
+
+    Audit findAuditsByBillBillId(Long billId);
+
+    Audit findAuditsByRequestRequestId(Long billId);
+
 }
