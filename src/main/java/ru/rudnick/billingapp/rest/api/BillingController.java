63f0e88a25e465156e34a4ddca55c09139ee5653
nrudopisov <nrudopisov@neoflex.ru>
2021-05-22 15:20:20 +0400

    Added methods for billing controller

--- src/main/java/ru/rudnick/billingapp/rest/api/BillingController.java
+++ src/main/java/ru/rudnick/billingapp/rest/api/BillingController.java
@@ -1,15 +1,27 @@
 package ru.rudnick.billingapp.rest.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import ru.rudnick.billingapp.entity.Bill;
+import ru.rudnick.billingapp.repository.BillRepository;
+
+import java.util.List;
 
 @RestController
 public class BillingController {
 
+    @Autowired
+    BillRepository repository;
 
-    @GetMapping()
-    public String test(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    @GetMapping("/bill/{id}")
+    public Bill getBill(@PathVariable Bill bill) {
+        return bill;
+    }
+
+    @GetMapping("/bills")
+    public List<Bill> getAllBills() {
+        return repository.findAll();
     }
 }
